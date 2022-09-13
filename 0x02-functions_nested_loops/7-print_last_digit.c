72e5e451562be259f8418a17b837ffb857a22340
David Oluwafemi Joshua <davidoluwafemi178@gmail.com>
2022-09-14 00:23:47 +0100

    Add more

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,13 +3,11 @@
  * print_last_digit - print last digit of integer
  * @n: integer argument
  * Return: last digit of number
- */
+*/
 
 int print_last_digit(int n)
 {
-  if (n < 0)
-    n = _abs(n);
+  n = _abs(n);
   _putchar('0' + (n % 10));
-
 	return (n % 10);
 }
